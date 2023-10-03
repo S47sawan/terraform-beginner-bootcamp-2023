ec17e2b69eb06a511c51abbed7744c6f037cf8fe
Smihah <87205154+S47sawan@users.noreply.github.com>
2023-10-03 10:57:50 +0100

    33 configuration drift (#34)
    * #33 removed random provider and random bucket name
    
    * #33 added trailing lines

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@
 #Create Outputs
 #-----------------------------------------------------------------------------------------------------------------------------
 output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  value = aws_s3_bucket.website_bucket.bucket
 }
