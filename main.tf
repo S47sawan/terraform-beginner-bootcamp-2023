102c4e80ef0d3bc2409a83a42b416673e9187502
Smihah Awan <smihah270@yahoo.com>
2023-10-03 20:09:24 +0000

    #35 refactor s3  bucket into new terrahouse s3 module

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
-#Create Resource s3 bucket
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+#backend terraform cloud
+# terraform {
+#   cloud {
+#     organization = "cloudgirl"
 
-  tags = {
-    UserUUID = "var.user_uuid"
-  }
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
--- providers.tf
+++ providers.tf
@@ -1,19 +1,20 @@
-#AWS Provider
-terraform {
-  cloud {
-    organization = "cloudgirl"
+# #AWS Provider
+# terraform {
+#   cloud {
+#     organization = "cloudgirl"
 
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-provider "aws" {
-  # Configuration options
-}
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+# }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.17.0"
+#     }
+#   }
+# }
+# provider "aws" {
+#   # Configuration options
+# }
