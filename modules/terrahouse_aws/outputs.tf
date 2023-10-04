d35fa75b6e3e8eefcae292f02f048bc2d800aeea
Smihah Awan <smihah270@yahoo.com>
2023-10-04 11:28:11 +0000

    #37configure s3 for website hosting and upload index and error.html file via tf.

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -3,5 +3,9 @@
 #-----------------------------------------------------------------------------------------------------------------------------
 output "bucket_name" {
   description = "Bucket name for static website hosting"
-  value = aws_s3_bucket.website_bucket.bucket
+  value       = aws_s3_bucket.website_bucket.bucket
+}
+output "website_endpoint" {
+  description = "s3 static website hosting endpoint"
+  value       = aws_s3_bucket_website_configuration.configuration_bucket.website_endpoint
 }
