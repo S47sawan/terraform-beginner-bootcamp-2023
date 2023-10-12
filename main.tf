0647cdf07b48ffcde50963de35b1607eb9ce5032
Smihah <87205154+S47sawan@users.noreply.github.com>
2023-10-12 13:50:28 +0100

    54 terratowns provider (#56)
    * #54 wip setting up the skeleton for creating a custom terraform provider
    
    * #54 added provider to the main.tf
    
    * #54 adding terraform provider to main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,7 @@ terraform {
       version = "1.0.0"
     }
   }
-  
 }
-
 #backend terraform cloud
 # terraform {
 #   cloud {
@@ -18,11 +16,11 @@ terraform {
 #     }
 #   }
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
-  
 }
+
 # module "terrahouse_aws" {
 #   source              = "./modules/terrahouse_aws"
 #   user_uuid           = var.user_uuid
