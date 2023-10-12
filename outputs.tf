a90d6b7150178eeec1d1315521e7ee97c9e10bf2
Smihah Awan <smihah270@yahoo.com>
2023-10-12 12:36:59 +0000

    #54 added provider to the main.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 #-----------------------------------------------------------------------------------------------------------------------------
 #Create Outputs
 #-----------------------------------------------------------------------------------------------------------------------------
-output "bucket_name" {
-  description = "Bucket name for static website hosting"
-  value       = module.terrahouse_aws.bucket_name
-}
-output "s3_website_endpoint" {
-  description = "s3 static website hosting endpoint"
-  value       = module.terrahouse_aws.website_endpoint
-}
-output "cloudfront_url" {
-  description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "bucket_name" {
+#   description = "Bucket name for static website hosting"
+#   value       = module.terrahouse_aws.bucket_name
+# }
+# output "s3_website_endpoint" {
+#   description = "s3 static website hosting endpoint"
+#   value       = module.terrahouse_aws.website_endpoint
+# }
+# output "cloudfront_url" {
+#   description = "The CloudFront Distribution Domain Name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
